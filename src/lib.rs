b8b7bd57528d8bdbb61e4b00aed074e16a0ecead
ozpv <39195175+ozpv@users.noreply.github.com>
2025-02-11 00:45:40 -0600

    add overridding default internal text

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,36 @@
+#![no_std]
+
+extern crate alloc;
 extern crate proc_macro;
 
+use alloc::string::String;
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, DeriveInput};
+use syn::{parse_macro_input, DeriveInput, Expr, Lit, Meta};
 
-#[proc_macro_derive(IntoResponse)]
+#[proc_macro_derive(IntoResponse, attributes(internal_text, status, text))]
 pub fn derive_into_response(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
 
     let name = input.ident;
+
+    let attrs = &input.attrs;
+
+    let internal_text = attrs
+        .iter()
+        .find(|attr| attr.path().is_ident("internal_text"))
+        .map(|attr| {
+            if let Meta::NameValue(meta) = &attr.meta {
+                if let Expr::Lit(expr) = &meta.value {
+                    if let Lit::Str(lit_str) = &expr.lit {
+                        return Some(lit_str.value());
+                    }
+                }
+            }
+            None
+        })
+        .flatten()
+        .unwrap_or_else(|| String::from("Something went wrong"));
 
     let expanded = quote! {
         impl ::axum::response::IntoResponse for #name {
@@ -19,13 +41,13 @@ pub fn derive_into_response(input: TokenStream) -> TokenStream {
 
                 let text = {
                     if status == ::axum::http::StatusCode::INTERNAL_SERVER_ERROR {
-                        String::from("Something went wrong")
+                        String::from(#internal_text)
                     } else {
                         self.to_string()
                     }
                 };
 
-                (status, text).into_response()
+                ::axum::response::IntoResponse::into_response((status, text))
             }
         }
     };
--- test/src/main.rs
+++ test/src/main.rs
@@ -1,22 +1,39 @@
 use axum::{response::IntoResponse, routing::get, Router};
-use axum_error_into_response_derive::IntoResponse;
+use axum_thiserror_intoresponse_derive::IntoResponse;
 use thiserror::Error;
 use tokio::net::TcpListener;
 
 #[derive(Debug, Error, IntoResponse)]
+// sets the default plain text response for StatusCode::INTERNAL_SERVER_ERROR
+#[internal_text = "overridden"]
 pub enum AppError {
+    // conditionally set StatusCode::INTERNAL_SERVER_ERROR response text with #[text("Something else")]
+    // or send the error message in plain text with #[err_text(true)]
+    // #[text = "Something else"]
+    // #[err_text(true)]
     #[error("This shouldn't show")]
-    Error,
+    Internal,
+    // automatically send the plain text when the error is set other than internal
+    // for example #[status(StatusCode::BAD_REQUEST)]
+    #[error("Bad request")]
+    ClientError,
 }
 
 async fn fail() -> impl IntoResponse {
-    AppError::Error
+    AppError::Internal
+}
+
+async fn client_fail() -> impl IntoResponse {
+    AppError::ClientError
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let listener = TcpListener::bind("127.0.0.1:3000").await?;
-    let routes = Router::new().route("/fail", get(fail));
+
+    let routes = Router::new()
+        .route("/fail", get(fail))
+        .route("/client_fail", get(client_fail));
 
     println!("Listening on http://127.0.0.1:3000/");
 
