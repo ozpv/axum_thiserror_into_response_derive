8beb5b134b713d0524c20cbe1d2e7bbd0376e423
ozpv <39195175+ozpv@users.noreply.github.com>
2025-02-11 02:50:13 -0600

    add status code attribute

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,10 @@
 extern crate alloc;
 extern crate proc_macro;
 
-use alloc::string::String;
+use alloc::{string::String, vec::Vec};
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, DeriveInput, Expr, Lit, Meta};
+use syn::{parse_macro_input, Data, DeriveInput, Expr, Lit, Meta};
 
 #[proc_macro_derive(IntoResponse, attributes(internal_text, status, text))]
 pub fn derive_into_response(input: TokenStream) -> TokenStream {
@@ -16,6 +16,7 @@ pub fn derive_into_response(input: TokenStream) -> TokenStream {
 
     let attrs = &input.attrs;
 
+    // parse the internal_text attribute
     let internal_text = attrs
         .iter()
         .find(|attr| attr.path().is_ident("internal_text"))
@@ -32,16 +33,46 @@ pub fn derive_into_response(input: TokenStream) -> TokenStream {
         .flatten()
         .unwrap_or_else(|| String::from("Something went wrong"));
 
+    // parse the attributes for status code override (if any)
+    let mut variant_overrides = Vec::new();
+
+    if let Data::Enum(data) = &input.data {
+        for variant in &data.variants {
+            let name = &variant.ident;
+
+            let attr = variant
+                .attrs
+                .iter()
+                .find(|attr| attr.path().is_ident("status"));
+
+            if let Some(attr) = attr {
+                if let Meta::List(list) = &attr.meta {
+                    let status = &list.tokens;
+
+                    let status = quote! {
+                        Self::#name => #status,
+                    };
+
+                    variant_overrides.push(status);
+                }
+            }
+        }
+    } else {
+        panic!("IntoResponse can only be derived on an Enum");
+    }
+
+    // build the impl
     let expanded = quote! {
         impl ::axum::response::IntoResponse for #name {
             fn into_response(self) -> ::axum::response::Response {
                 let status = match self {
+                    #(#variant_overrides),*
                     _ => ::axum::http::StatusCode::INTERNAL_SERVER_ERROR,
                 };
 
                 let text = {
                     if status == ::axum::http::StatusCode::INTERNAL_SERVER_ERROR {
-                        String::from(#internal_text)
+                        #internal_text.to_string()
                     } else {
                         self.to_string()
                     }
@@ -52,5 +83,5 @@ pub fn derive_into_response(input: TokenStream) -> TokenStream {
         }
     };
 
-    TokenStream::from(expanded)
+    expanded.into()
 }
--- test/src/main.rs
+++ test/src/main.rs
@@ -1,11 +1,11 @@
-use axum::{response::IntoResponse, routing::get, Router};
+use axum::{http::StatusCode, response::IntoResponse, routing::get, Router};
 use axum_thiserror_intoresponse_derive::IntoResponse;
 use thiserror::Error;
 use tokio::net::TcpListener;
 
-#[derive(Debug, Error, IntoResponse)]
 // sets the default plain text response for StatusCode::INTERNAL_SERVER_ERROR
-#[internal_text = "overridden"]
+// #[internal_text = "overridden"]
+#[derive(Debug, Error, IntoResponse)]
 pub enum AppError {
     // conditionally set StatusCode::INTERNAL_SERVER_ERROR response text with #[text("Something else")]
     // or send the error message in plain text with #[err_text(true)]
@@ -14,7 +14,7 @@ pub enum AppError {
     #[error("This shouldn't show")]
     Internal,
     // automatically send the plain text when the error is set other than internal
-    // for example #[status(StatusCode::BAD_REQUEST)]
+    #[status(StatusCode::BAD_REQUEST)]
     #[error("Bad request")]
     ClientError,
 }
@@ -32,8 +32,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let listener = TcpListener::bind("127.0.0.1:3000").await?;
 
     let routes = Router::new()
-        .route("/fail", get(fail))
-        .route("/client_fail", get(client_fail));
+        .route("/", get(fail))
+        .route("/bad", get(client_fail));
 
     println!("Listening on http://127.0.0.1:3000/");
 
